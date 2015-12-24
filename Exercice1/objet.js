ed1b0e00e8df05e29190f065cdabf99144dfa6a3
svenisis1 <kazumasis2001@gmail.com>
2015-12-24 12:09:13 +0000

    rajout

--- Exercice1/objet.js
+++ Exercice1/objet.js
@@ -1,12 +1,18 @@
-                                                    
-
+                                                $liste = document.getElementById("liste");
+$prenom = document.getElementById("prenom");
+$nom = document.getElementById("nom");
+$numero = document.getElementById("numero");
+$ajouter = document.getElementById("ajouter");
+$recherche = document.getElementById("recherche");
+$rechercher = document.getElementById("rechercher");
+$resultats = document.getElementById("resultats");
 
 contacts = [{
-    prenom: "Valérian",
-    nom: "Michel",
+    prenom: "Michel",
+    nom: "David",
     numero: "0123456789"
 }, {
-    prenom: "David",
+    prenom: "Valérian",
     nom: "Smith",
     numero: "0123456789"
 }];
@@ -15,4 +21,34 @@ function texteContact(contact) {
     return contact.prenom + " " + contact.nom + " (" + contact.numero + ")";
 }
 
-alert(texteContact(contacts[0]));
+function afficherListe() {
+    elements = "";
+    for (i = 0; i < contacts.length; i++) {
+        elements += "<li>" + texteContact(contacts[i]) + "</li>";
+    }
+    $liste.innerHTML = elements;
+}
+
+function ajouter() {
+    contacts.push({
+        prenom: $prenom.value,
+        nom: $nom.value,
+        numero: $numero.value
+    });
+    afficherListe();
+}
+
+function rechercher() {
+    recherche = $recherche.value;
+    elements = "";
+    for (i = 0; i < contacts.length; i++) {
+        if (contacts[i].prenom == recherche || contacts[i].nom == recherche || contacts[i].numero == recherche) {
+            elements += "<li>" + texteContact(contacts[i]) + "</li>";
+        }
+    }
+    $resultats.innerHTML = elements;
+}
+
+$ajouter.onclick = ajouter;
+$rechercher.onclick = rechercher;
+afficherListe();
